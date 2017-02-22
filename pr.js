bccc58dee6f48a2e2eaed76b7b9c6362db6ff024
Admin <vanpho02@gmail.com>
2017-02-22 20:43:16 +0700

    add pr

--- pr.js
+++ pr.js
@@ -1,11 +1,17 @@
-let aPromise = new Promise((resolve, reject) => {
-  // resolve();
-  reject()
-});
+let add = (a, b) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if(typeof a != 'number' || typeof b != 'number'){
+        return reject(new Error('Tham so phai co kieu number'))
+      }
+      resolve(a + b);
+    }, 1000);
+  });
+}
 
-aPromise.then(
-  () => console.log('Thanh cong'),
-  () => console.log('That bai')
+add(4, 5).then(
+  (res) => console.log('Thanh cong:', res),
+  (err) => console.log('That bai', err)
 );
 
-console.log('Ket thuc');
+// console.log('Ket thuc');

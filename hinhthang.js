dedc58ccb4df55141c558ffc05e61b1b5e9f55ae
Admin <vanpho02@gmail.com>
2017-02-22 20:02:16 +0700

    add mul div

--- hinhthang.js
+++ hinhthang.js
@@ -11,6 +11,25 @@ let add = (a, b, cb) => {
   }, 1000);
 }
 
+let mul = (a, b, cb) => {
+  setTimeout(() => {
+    if(typeof a != 'number' && typeof b != 'number'){
+      return cb(new Error('Tham so phai co kieu number'))
+    }
+    cb(undefined, a * b);
+  }, 1000);
+}
+
+let div = (a, b, cb) => {
+  setTimeout(() => {
+    if(typeof a != 'number' && typeof b != 'number'){
+      return cb(new Error('Tham so phai co kieu number'))
+    }
+    if(b == 0) return cb(new Error('Chia cho 0'));
+    cb(undefined, a / b);
+  }, 1000);
+}
+
 add(1, 2, (err, result) => {
   if(err) return console.log(err);
   console.log('Ket qua:', result);
